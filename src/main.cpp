ee8e6b79d75dba2823a21af2793065edf74a726b
Adrian Cheater <adrian@complexgames.com>
2014-01-19 16:36:15 -0600

    Removed auto-tools, making Makefile by hand. Basic functionality implemented in OpenSceneGraph

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@
 #include "renderer/renderable.hpp"
 #else
 #include <iostream>
+#include <osg/Node>
+#include <osg/Image>
+#include <osgDB/ReadFile>
+#include <osgViewer/Viewer>
+#include <osgGA/KeySwitchMatrixManipulator>
+#include <osg/Texture2D>
 #endif
 
 void glErrorLogger( int code, const char* description )
@@ -50,6 +56,24 @@ void runAdrianRenderer()
 void runOSGRenderer()
 {
   std::cout << "Starting free allgiance 0.0.1\n";
+  osgViewer::Viewer viewer;
+
+  osg::ref_ptr<osg::Group> root( new osg::Group );
+
+  osg::ref_ptr<osg::ClearNode> cNode( new osg::ClearNode );
+  cNode->setClearColor(osg::Vec4(0.0f,0.0f,0.0f,1.0f));
+//  root->addChild(cNode.get());
+
+  osg::ref_ptr<osg::Node> model = osgDB::readNodeFile("../data/suzanne.obj" );
+  osg::ref_ptr<osg::Image> img = osgDB::readImageFile("../data/img.png" );
+  osg::ref_ptr<osg::Texture2D> tex = new osg::Texture2D(img);
+  model->getOrCreateStateSet()->setTextureAttributeAndModes( 0, tex.get(), osg::StateAttribute::ON );
+  root->addChild(model.get());
+
+  osg::ref_ptr<osgGA::KeySwitchMatrixManipulator> cameraManip( new osgGA::KeySwitchMatrixManipulator );
+  viewer.setSceneData(root.get());
+  viewer.run();
+
   std::cout << "Stopping free allgiance 0.0.1\n";
 }
 #endif
